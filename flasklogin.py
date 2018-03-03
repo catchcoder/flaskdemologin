28eb2e2fdd12e2422731c69a55df992b014e67b5
Chris Hawkins <catchcoder@gmail.com>
2018-03-03 20:42:59 +0000

    added password db with uuid

--- flasklogin.py
+++ flasklogin.py
@@ -1,4 +1,5 @@
 # import os
+import uuid
 
 from flask import Flask, render_template, flash, request, url_for, redirect, session
 from flask_sqlalchemy import SQLAlchemy
@@ -17,46 +18,58 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 db = SQLAlchemy(app)
+db.create_all()
 db.init_app(app)
 
 class Username(db.Model):
     __tablename__ = 'users'
     id = db.Column('usernames_id', db.Integer, primary_key = True)
-    username = db.Column(db.String(100))
-    email = db.Column(db.String(60))
-    password = db.Column(db.String(50))
+    username = db.Column('username', db.String(100))
+    email = db.Column('email', db.String(60))
+    password = db.Column('password', db.String(50))
+    accountlive = db.Column('accountlive', db.Boolean, nullable=False, default=False)
+    emailcheckcode = db.Column('emailcheckcode', db.String(36))
 
-    def __init__(self, username, email, password):
+
+    def __init__(self, username, email, password, accountlive, emailcheckcode):
         self.username = username
         self.email = email
         self.password = password
+        self.accountlive = accountlive
+        self.emailcheckcode = emailcheckcode
+
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        username = session['username']
+    if 'email' in session:
+        email = session['email']
     else:
-        username = None
-    return render_template('index.html',user = username)
+        email = None
+    return render_template('index.html',email = email)
 
 
 @app.route('/login', methods = ['GET', 'POST'] )
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        id = request.args.get('login')
+
+        return render_template('login.html',id = id)
     elif request.method == 'POST':
-        usernametocheck = request.form['username']
+        emailtocheck = request.form['email']
         passwordtocheck = request.form['password']
 
         # user = Username.query.filter_by(username=usernametocheck).first()
-         user = Username.query.filter_by(username=usernametocheck).first()
+        user = Username.query.filter_by(email=emailtocheck).first()
         if user is None:
-            return "Not Found"
+            return redirect(url_for('login', login='failed'))
+        if passwordtocheck == user.password:
+            session['email'] = emailtocheck
+            return redirect(url_for('index'))
         else:
-            return "Found " + request.form['username']
+            return redirect(url_for('login',login='failed'))
         # session['username'] = usernametocheck
 
-        return redirect(url_for('index'))
+        return redirect(url_for('login', login='failed'))
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -68,10 +81,10 @@ def register():
         emailconfirm = request.form['emailconfirm']
         password = request.form['password']
         passwordconfirm = request.form['passwordconfirm']
+        emailcheckcode = str(uuid.uuid1())
 
         #flash ('username'+ username + 'email' + email + 'password' + password)
-        newuser = Username(username, email, password)
-
+        newuser = Username(username, email, password, False, emailcheckcode)
         db.session.add(newuser)
         db.session.commit()
 
