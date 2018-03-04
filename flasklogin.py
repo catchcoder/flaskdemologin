74bab0ea426ebf82d58ff6378e0ead2cfc644187
Chris Hawkins <catchcoder@gmail.com>
2018-03-04 17:36:20 +0000

    includes email signup check

--- flasklogin.py
+++ flasklogin.py
@@ -1,21 +1,44 @@
-# import os
+import os
 import uuid
+import yaml
 
 from flask import Flask, render_template, flash, request, url_for, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
+from flask_mail import Mail, Message
+from urllib import parse
 
-# basedir = os.path.abspath(os.path.dirname(__file__))
+# Load yaml file with config setting
+# basedir = os.getcwd()
+# with open(os.path.join(basedir, 'config.yml'), 'r') as ymlfile:'
+with open('config.yml', 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///flasklogin.sqlite3'
 
+# Flask app and secret
 app = Flask(__name__)
 app.secret_key = "04dc6238-1d87-11e8-b2ed-e82aea18273e"
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+
+#Mailer
+mail=Mail(app)
+
+app.config.update(
+	DEBUG=True,
+	#EMAIL SETTINGS
+	MAIL_SERVER= cfg['mail']['MAIL_SERVER'],
+	MAIL_PORT= cfg['mail']['MAIL_PORT'],
+	MAIL_USE_SSL=True,
+	MAIL_USERNAME = cfg['mail']['MAIL_USERNAME'],
+	MAIL_PASSWORD = cfg['mail']['MAIL_PASSWORD']
+)
+
+mail=Mail(app)
+
+
+# Database connectiona and table
+app.config['SQLALCHEMY_DATABASE_URI'] = cfg['database']['SQLALCHEMY_DATABASE_URI']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
 
 db = SQLAlchemy(app)
 db.create_all()
@@ -38,7 +61,7 @@ class Username(db.Model):
         self.accountlive = accountlive
         self.emailcheckcode = emailcheckcode
 
-
+# Routes
 @app.route('/')
 def index():
     if 'email' in session:
@@ -62,6 +85,8 @@ def login():
         user = Username.query.filter_by(email=emailtocheck).first()
         if user is None:
             return redirect(url_for('login', login='failed'))
+        if not user.accountlive:
+            return redirect(url_for('login', login='emailnotverified'))
         if passwordtocheck == user.password:
             session['email'] = emailtocheck
             return redirect(url_for('index'))
@@ -74,23 +99,36 @@ def login():
 @app.route('/register', methods=['GET','POST'])
 def register():
     if request.method == 'GET':
-        return render_template('register.html')
+        id = request.args.get('id')
+        return render_template('register.html', id=id)
     elif request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
-        emailconfirm = request.form['emailconfirm']
+        #emailconfirm = request.form['emailconfirm']
         password = request.form['password']
-        passwordconfirm = request.form['passwordconfirm']
+        #passwordconfirm = request.form['passwordconfirm']
         emailcheckcode = str(uuid.uuid1())
 
-        #flash ('username'+ username + 'email' + email + 'password' + password)
-        newuser = Username(username, email, password, False, emailcheckcode)
-        db.session.add(newuser)
-        db.session.commit()
+        user = Username.query.filter_by(email=email).first()
+        if user is None:
+            #flash ('username'+ username + 'email' + email + 'password' + password)
+            newuser = Username(username, email, password, False, emailcheckcode)
+            db.session.add(newuser)
+            db.session.commit()
 
+            confirmurl = "http://localhost/" + parse.quote(email) + "/" + emailcheckcode
 
-        return redirect(url_for('index'))
-        # return redirect(url_for('index.html'))
+            msg = Message(subject="FlaskLogin confirm registation",
+                          body= confirmurl,
+                          sender="Register@flasklogin.com",
+                          recipients=[email])
+
+            mail.send(msg)
+
+            return redirect(url_for('login', id='emailsent'))
+        else:
+            return redirect(url_for('register', id='emailexist'))
+        # return redirect(url_for('index.html'))r
 
 @app.route('/logoff')
 def logoff():
@@ -98,6 +136,25 @@ def logoff():
         session.pop('username',None)
     return redirect(url_for('index'))
 
+
+@app.route('/<email>/<authcheck>')
+def authcheck(email, authcheck):
+
+    user = Username.query.filter_by(email=email).first()
+    if user is None:
+        return ('not in db')
+
+    if user.emailcheckcode == authcheck:
+        user.accountlive = True
+        db.session.commit()
+        return ('yes it checked out')
+
+    else:
+        return ('sadly it doesn\'t match')
+
+
+
+    return ("email {} \br authcode {}".format(emailaddress,authcode))
 
 
 if __name__ == '__main__':
